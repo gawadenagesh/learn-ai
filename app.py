2788a55a4364390ec76b2acd46fc53407b70f948
Nagesh Gawade <nageshgawade@Nageshs-Air.elecom>
2025-08-30 20:15:35 +0530

    Changed Model path.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open("pickle/model.pkl", "rb") as file:
+with open("model.pkl", "rb") as file:
     model = pickle.load(file)
 
 # Define feature names and their limits
